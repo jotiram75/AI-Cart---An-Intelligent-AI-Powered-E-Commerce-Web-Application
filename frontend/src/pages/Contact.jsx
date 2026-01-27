206e112f5ec42befac40985167d2ccf24cac485c
Jotiram75 <shindejotiram13032003@gmail.com>
2026-01-27 23:48:05 +0530

    hooks errors

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,14 +19,14 @@ function Contact() {
           <p className="lg:w-[80%] w-[100%] text-[white] font-bold lg:text-[18px] text-[15px]">
             Our Store
           </p>
-          <p className="lg:w-[80%] w-[100%] text-[white] md:text-[16px] text-[13px]">
+          <div className="lg:w-[80%] w-[100%] text-[white] md:text-[16px] text-[13px]">
             <p>12345 Random Station</p>
             <p>random city , state , India</p>
-          </p>
-          <p className="lg:w-[80%] w-[100%] text-[white] md:text-[16px] text-[13px]">
+          </div>
+          <div className="lg:w-[80%] w-[100%] text-[white] md:text-[16px] text-[13px]">
             <p>tel: +91-8767011846</p>
             <p>Email: admin@aicart.com</p>
-          </p>
+          </div>
           <p className="lg:w-[80%] w-[100%] text-[15px] text-[white] lg:text-[18px] mt-[10px] font-bold">
             Careers at AICART
           </p>
