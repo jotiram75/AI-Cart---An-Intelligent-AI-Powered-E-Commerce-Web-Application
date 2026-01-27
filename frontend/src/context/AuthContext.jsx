c331acbfbd4405e383e3d09f5a328a644122b48b
Jotiram75 <shindejotiram13032003@gmail.com>
2026-01-27 08:14:43 +0530

    rename file

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import { createContext } from 'react'
-export const authDataContext= createContext()
-function AuthContext({children}) {
-    let serverUrl = import.meta.env.VITE_SERVER_URL
+import React from "react";
+import { createContext } from "react";
+export const authDataContext = createContext();
+function AuthContext({ children }) {
+  let serverUrl = import.meta.env.VITE_SERVER_URL;
 
-    let value = {
-       serverUrl
-    }
+  let value = {
+    serverUrl,
+  };
   return (
-
-    
     <div>
-        <authDataContext.Provider value={value}>
-            {children}
-        </authDataContext.Provider>
-      
+      <authDataContext.Provider value={value}>
+        {children}
+      </authDataContext.Provider>
     </div>
-  )
+  );
 }
 
-export default AuthContext
+export default AuthContext;
