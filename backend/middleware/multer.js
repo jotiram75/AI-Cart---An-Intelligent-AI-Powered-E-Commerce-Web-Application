91137136d828989efefe83c33e3b3d4ba289bf9d
Jotiram75 <shindejotiram13032003@gmail.com>
2026-02-01 11:40:40 +0530

    addproduct issue solve

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -2,7 +2,8 @@ import multer from 'multer'
 
 let storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,"./public")
+        const start = process.env.NODE_ENV === 'production' ? '/tmp' : './public'; 
+        cb(null,start)
     },
     filename:(req,file,cb)=>{
         cb(null,file.originalname)
